1a8bdfd1b460ffc82176a2a0538633d41c0c66f0
Idhar Firmansyah <interisty91@gmail.com>
2021-11-27 22:50:03 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -5,12 +5,12 @@ function getDatabaseConfig()
     return [
         "database" => [
             "dev" => [
-                "url" => "mysql:host=localhost:3306;dbname=pzn_login_test",
+                "url" => "mysql:host=localhost:3306;dbname=db_dev",
                 "username" => "root",
                 "password" => ""
             ],
             "prod" => [
-                "url" => "mysql:host=localhost:3306;db_name=pzn_login",
+                "url" => "mysql:host=localhost:3306;db_name=db_prod",
                 "username" => "root",
                 "password" => ""
             ]
